3061dea655ac1e49c26663c2a4f2233747d7c7cc
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2024-02-27 19:04:29 +0530

    page and limit added

--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -1,17 +1,21 @@
-
-const express = require("express")
-const router = express.Router()
-const Token = require("../model/tokenModel")
+const express = require("express");
+const router = express.Router();
+const Token = require("../model/tokenModel");
 
 router.get("/tokens", async (req, res) => {
+    const { page, limit } = req.query;
+    const pageNo = parseInt(page) || 1;
+    const limitNo = parseInt(limit) || 6;
+    const skipCount = (pageNo - 1) * limitNo;
+
     try {
-        let data = await Token.find({})
+        const data = await Token.find({}).skip(skipCount).limit(limitNo);
         console.log(data);
-        res.status(200).json({ data: data })
+        res.status(200).json({ data: data });
     } catch (error) {
         console.log(error);
-        res.status(500).json('internal server error')
+        res.status(500).json('Internal Server Error');
     }
-})
+});
 
-module.exports = router
+module.exports = router;
